02916bf3ebabfee15cddc495a07acd76503ffa00
hadri12velay <hadri1.2velay@gmail.com>
2023-05-23 22:37:12 +0100

    [links & projects] update to style

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,31 +1,55 @@
 import React from "react";
 import { ReactComponent as LinkedinLogo } from "../assets/linkedin.svg";
-import { ReactComponent as InstagramLogo } from "../assets/instagram.svg";
-import { ReactComponent as ResumeLogo } from "../assets/resume.svg";
+import { ReactComponent as InstagramLogo } from "../assets/instagram2.svg";
+import { ReactComponent as ResumeLogo } from "../assets/resume2.svg";
 import { ReactComponent as GithubLogo } from "../assets/github.svg";
 import { ReactComponent as EmailLogo } from "../assets/email.svg";
 
 function Links() {
-  return (
-    <>
-      <h2>links</h2>
-      <a href="https://github.com/hadri12velay">
-        <GithubLogo />
-      </a>
-      <a href="https://www.linkedin.com/in/hbed-develay/">
-        <LinkedinLogo />
-      </a>
-      <a href="https://www.linkedin.com/in/hbed-develay/">
-        <InstagramLogo />
-      </a>
-      <a href="mailto: hadrien@develay.uk">
-        <EmailLogo />
-      </a>
-      <a href="https://www.linkedin.com/in/hbed-develay/">
-        <ResumeLogo />
-      </a>
-    </>
-  );
+    return (
+        <>
+            <a
+                href="https://github.com/hadri12velay"
+                className="github"
+                target="_blank"
+                title="GitHub"
+            >
+                <GithubLogo />
+            </a>
+            <a
+                href="https://www.linkedin.com/in/hbed-develay/"
+                className="linkedin"
+                target="_blank"
+                title="LinkedIn"
+            >
+                <LinkedinLogo />
+            </a>
+            <a
+                href="https://www.instagram.com/hadri1.2velay/"
+                className="instagram"
+                target="_blank"
+                title="Instagram"
+            >
+                <InstagramLogo />
+            </a>
+            <a
+                href="mailto: hadrien@develay.uk"
+                className="email"
+                target="_blank"
+                title="e-mail"
+            >
+                <EmailLogo />
+            </a>
+            <a
+                href="https://hadri1.com/resume/"
+                className="resume"
+                target="_blank"
+                title="resume"
+            >
+                <ResumeLogo />
+            </a>
+        </>
+    );
 }
 
 export default Links;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,41 +1,46 @@
 import React from "react";
 
 function Projects({
-  projects: projects,
-  selectedProj: selectedProj,
-  setSelectedProj: setSelectedProj,
+    projects: projects,
+    selectedProj: selectedProj,
+    setSelectedProj: setSelectedProj,
 }) {
-  const handleProjClick = (e) => {
-    setSelectedProj(Number(e.currentTarget.dataset.id));
-  };
+    const handleProjClick = (e) => {
+        setSelectedProj(Number(e.currentTarget.dataset.id));
+    };
 
-  console.log("selectedProj: ", selectedProj);
+    console.log("selectedProj: ", selectedProj);
 
-  return (
-    <>
-      <h2>projects</h2>
-      {projects.map((project, index) => (
-        <div
-          key={index}
-          data-id={index}
-          onClick={(e) => {
-            handleProjClick(e);
-          }}
-        >
-          <p className={index === selectedProj ? "selected" : ""}>
-            <span className="project-name">-&gt; {project.name}{project.new && <span className="new-tag">new</span>}</span>
-            <span className="tech-container">
-              {projects[index].tech.map((thing, index) => (
-                <span className="tech" key={index}>
-                  {thing}
-                </span>
-              ))}
-            </span>
-          </p>
-        </div>
-      ))}
-    </>
-  );
+    return (
+        <>
+            <h2>projects</h2>
+            {projects.map((project, index) => (
+                <div
+                    key={index}
+                    data-id={index}
+                    onClick={(e) => {
+                        handleProjClick(e);
+                    }}
+                >
+                    <p className={index === selectedProj ? "selected" : ""}>
+                        <span className="project-name">
+                            -&gt; {project.name}
+                            {project.new && (
+                                <span className="new-tag">new</span>
+                            )}
+                        </span>
+                        <span className="tech-container">
+                            {projects[index].tech.map((thing, index) => (
+                                <span className="tech" key={index}>
+                                    {thing}
+                                </span>
+                            ))}
+                        </span>
+                    </p>
+                </div>
+            ))}
+        </>
+    );
 }
 
 export default Projects;
