c5393db35d19351e76d469900a808c74dd7aa131
hadri12velay <79649164+hadri12velay@users.noreply.github.com>
2023-05-23 17:56:27 +0100

    [links] added a bunch of links

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { ReactComponent as LinkedinLogo } from "../assets/linkedin.svg";
+import { ReactComponent as InstagramLogo } from "../assets/instagram.svg";
+import { ReactComponent as ResumeLogo } from "../assets/resume.svg";
+import { ReactComponent as GithubLogo } from "../assets/github.svg";
+import { ReactComponent as EmailLogo } from "../assets/email.svg";
 
 function Links() {
   return (
     <>
       <h2>links</h2>
+      <a href="https://github.com/hadri12velay">
+        <GithubLogo />
+      </a>
       <a href="https://www.linkedin.com/in/hbed-develay/">
         <LinkedinLogo />
+      </a>
+      <a href="https://www.linkedin.com/in/hbed-develay/">
+        <InstagramLogo />
+      </a>
+      <a href="mailto: hadrien@develay.uk">
+        <EmailLogo />
+      </a>
+      <a href="https://www.linkedin.com/in/hbed-develay/">
+        <ResumeLogo />
       </a>
     </>
   );
